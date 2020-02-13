3ce90779cebad7509afbdb0ad6ed06bacb031464
Requena21 <Requena21@users.noreply.github.com>
2020-02-13 14:22:09 +0100

    Controlador en primera persona

---
+++ PDV_GAME/Assets/MoverCamara.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class MoverCamara : MonoBehaviour
+{
+
+    public Transform player;
+
+    void Update()
+    {
+        transform.position = player.transform.position;
+    }
+}
