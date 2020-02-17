720f786e2e0535221b932832867c79b3e8edc7bf
Requena21 <Requena21@users.noreply.github.com>
2020-02-17 01:52:32 +0100

    Disparos

---
+++ PDV_GAME/Assets/Proyecto/Scripts/Arma1.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Arma1 : MonoBehaviour
+{
+    public float impactForce = 30.0f;
+    public float damage = 10.0f;
+    public float range = 100.0f;
+    public Camera fpsCam;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+
+
+    }
+
+    void shoot()
+    {
+        RaycastHit hitInfo;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, range))
+        {
+            Debug.Log(hitInfo.transform.name);
+            Target target = hitInfo.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            if (hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(-hitInfo.normal * impactForce);
+            }
+        }
+
+
+    }
+}
